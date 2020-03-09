2bee2b4d78ddd50b3e323ffb55f12595b2dd18ba
Leah Barnes <johnson.leahe@gmail.com>
2020-03-08 20:49:16 -0400

    Setting up components, pages, app.js.

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import About from "./pages/About";
+import Connect from "./pages/Connect";
+import Home from "./pages/Home";
+import Portfolio from "./pages/Portfolio";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Projects from "./components/Projects";
+import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={About} />
+          <Route exact path="/About" component={About} />
+          <Route exact path="/Connect" component={Connect} />
+          <Route exact path="/Home" component={Home} />
+          <Route exact path="/Portfolio" component={Portfolio} />
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
