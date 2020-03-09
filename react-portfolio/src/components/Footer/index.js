9e0cd346d0f26b220a45f92db1456b9e371ec09a
Leah Barnes <johnson.leahe@gmail.com>
2020-03-08 21:13:51 -0400

    Adding footer and footer styling.

---
+++ react-portfolio/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <span>Leah Barnes © 2019</span>
+    </footer>
+  );
+}
+
+export default Footer;
